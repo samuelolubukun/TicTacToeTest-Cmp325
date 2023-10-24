5b0ba30d6639b20f444ecfee415be27575c7a395
samuelolubukun <samuel.olubukun@binghamuni.edu.ng>
2023-10-24 11:56:01 -0700

    First Commit

--- src/test/TicTacToeTest.java
+++ src/test/TicTacToeTest.java
@@ -15,12 +15,65 @@ public class TicTacToeTest {
     }
 
     @Test
+    public void testCheckWinNoWin() {
+        char[][] board = {
+                {'X', 'O', 'X'},
+                {'O', 'X', 'O'},
+                {'O', 'X', 'O'}
+        };
+        ticTacToe = new TicTacToe();
+        ticTacToe.board = board;
+        char result = ticTacToe.checkWin();
+        assertEquals(' ', result);
+    }
+
+    @Test
     public void testCheckWinHorizontalWin() {
         char[][] board = {
                 {'X', 'X', 'X'},
                 {'O', 'O', ' '},
                 {' ', ' ', 'O'}
         };
+        ticTacToe = new TicTacToe();
+        ticTacToe.board = board;
+        char result = ticTacToe.checkWin();
+        assertEquals('X', result);
+    }
+
+    @Test
+    public void testCheckWinVerticalWin() {
+        char[][] board = {
+                {'X', 'O', 'X'},
+                {'X', 'O', ' '},
+                {'X', ' ', 'O'}
+        };
+        ticTacToe = new TicTacToe();
+        ticTacToe.board = board;
+        char result = ticTacToe.checkWin();
+        assertEquals('X', result);
+    }
+
+    @Test
+    public void testCheckWinDiagonalWin1() {
+        char[][] board = {
+                {'X', 'O', 'X'},
+                {'O', 'X', 'O'},
+                {'O', ' ', 'X'}
+        };
+        ticTacToe = new TicTacToe();
+        ticTacToe.board = board;
+        char result = ticTacToe.checkWin();
+        assertEquals('X', result);
+    }
+
+    @Test
+    public void testCheckWinDiagonalWin2() {
+        char[][] board = {
+                {'O', 'O', 'X'},
+                {'O', 'X', 'O'},
+                {'X', ' ', 'O'}
+        };
+        ticTacToe = new TicTacToe();
         ticTacToe.board = board;
         char result = ticTacToe.checkWin();
         assertEquals('X', result);
@@ -33,31 +86,10 @@ public class TicTacToeTest {
                 {'X', 'O', 'O'},
                 {'O', 'X', 'X'}
         };
+        ticTacToe = new TicTacToe();
         ticTacToe.board = board;
         char result = ticTacToe.checkWin();
         assertEquals(' ', result);
-    }
-
-    @Test
-    public void testReset() {
-        char[][] board = {
-                {'X', 'O', 'X'},
-                {'X', 'O', 'O'},
-                {'O', 'X', 'X'}
-        };
-        ticTacToe.board = board;
-        ticTacToe.reset();
-        char[][] resetBoard = ticTacToe.getBoard();
-        char expectedCurrentPlayer = 'X';
-
-        char[][] expectedBoard = {
-                {' ', ' ', ' '},
-                {' ', ' ', ' '},
-                {' ', ' ', ' '}
-        };
-
-        assertArrayEquals(expectedBoard, resetBoard);
-        assertEquals(expectedCurrentPlayer, ticTacToe.getCurrentPlayer());
     }
 
     @Test
@@ -67,8 +99,33 @@ public class TicTacToeTest {
                 {'X', 'O', 'O'},
                 {'O', 'X', 'X'}
         };
+        ticTacToe = new TicTacToe();
         ticTacToe.board = board;
         boolean result = ticTacToe.isDraw();
         assertTrue(result);
+    }
+
+    @Test
+    public void testReset() {
+        char[][] board = {
+                {'X', 'O', 'X'},
+                {'X', 'O', 'O'},
+                {'O', 'X', 'X'}
+        };
+        ticTacToe = new TicTacToe();
+        ticTacToe.board = board;
+        ticTacToe.reset();
+        char[][] resetBoard = ticTacToe.getBoard();
+        char currentPlayer = ticTacToe.getCurrentPlayer();
+
+        char[][] expectedBoard = {
+                {' ', ' ', ' '},
+                {' ', ' ', ' '},
+                {' ', ' ', ' '}
+        };
+        char expectedCurrentPlayer = 'X';
+
+        assertArrayEquals(expectedBoard, resetBoard);
+        assertEquals(expectedCurrentPlayer, currentPlayer);
     }
 }
